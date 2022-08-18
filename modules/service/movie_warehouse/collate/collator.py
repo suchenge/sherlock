19806f58e111960b5b2c8e239c911b1f78e41e0d
VITO-PC\宿远 <vito.su@live.com>
2022-08-18 23:23:41 +0800

    完善功能

--- modules/service/movie_warehouse/collate/collator.py
+++ modules/service/movie_warehouse/collate/collator.py
@@ -1,5 +1,7 @@
 import os
 import queue
+import sys
+import traceback
 
 from modules.service.movie_warehouse.collate.porter import Porter
 from modules.service.movie_warehouse.collate.marauder import marauder_factory
@@ -30,8 +32,9 @@ class Collator(object):
               % (file.name, file.type, file.title, file.folder, file.path))
 
         if file.type == 'torrent' and dictionary.exists(file.title):
-            pass
+            os.remove(file.path)
         else:
+            film = None
             try:
                 marauder = marauder_factory.get_marauder(**{'file': file, 'request': self.__request__})
                 film = marauder.to_film()
@@ -48,6 +51,19 @@ class Collator(object):
 
             except Exception as error:
                 self.__exceptions__.put(error)
+                try:
+                    if file is not None and file.type == 'torrent':
+                        exception_path = str.replace(file.path, file.type, 'exception.' + file.type)
+                        os.rename(file.path, exception_path)
+
+                        if film is not None:
+                            if os.path.exists(film.folder):
+                                os.remove(film.folder)
+
+                except Exception as error1:
+                    self.__exceptions__.put(error1)
+
+
 
         print("处理文件完成 " + file.path)
 
@@ -60,10 +76,19 @@ class Collator(object):
 
         has_errors = not self.__exceptions__.empty()
         while not self.__exceptions__.empty():
-            print(self.__exceptions__.get())
+            e = self.__exceptions__.get()
+
+            print('str(Exception):\t', str(Exception))
+            print('str(e):\t\t', str(e))
+            print('repr(e):\t', repr(e))
+
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print('e.message:\t', exc_value)
+            print("Note, object e and exc of Class %s is %s the same." % (type(exc_value), ('not', '')[exc_value is e]))
+            print('traceback.print_exc(): ', traceback.print_exc())
+            print('traceback.format_exc():\n%s' % traceback.format_exc())
 
         if has_errors:
             os.system("pause")
         else:
             print('全部完成')
-
