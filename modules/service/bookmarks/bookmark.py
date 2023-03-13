46d6e6a40f3f7d2541aa45ba17539cbf119a7a01
Vito Su <vitosu@qt-asia.com>
2023-03-13 13:29:15 +0800

    重新调整bookmark状态保存机制

--- modules/service/bookmarks/bookmark.py
+++ modules/service/bookmarks/bookmark.py
@@ -5,8 +5,6 @@ import shutil
 from pathlib import Path
 
 from modules.tools.http_request.http_client import HttpClient
-from modules.tools.thread_pools.task import Task
-from modules.tools.thread_pools.task_pool import TaskPool
 
 from modules.service.movie_marauder.movie_information import MovieInformation
 from modules.service.movie_marauder.javdb_marauder import JavdbMarauder
@@ -22,77 +20,59 @@ class Bookmark(object):
         self.__index__ = kwargs["index"]
         self.__status__ = kwargs["status"]
 
-        if kwargs.get("group_path"):
-            self.__group_path__ = kwargs["group_path"]
-
         self.__path__ = None
         self.__information_file_name__ = "information.json"
-        self.__information_file_path__ = None
-        self.__information__ = None
 
-        if kwargs.get("path"):
-            self.__path__ = kwargs["path"]
-            self.__information_file_path__ = os.path.join(self.__path__, self.__information_file_name__)
-            self.__information__ = self.__get_information_by_file__()
-
-        self.__inspection_delay_seconds__ = 0
-        self.__inspection_count__ = 0
-
-        self.__group__ = None
-
-    @property
-    def group(self):
-        return self.__group__
-
-    @group.setter
-    def group(self, value):
-        self.__group__ = value
 
     @property
     def status(self) -> str:
         return self.__status__
 
-    def __get_information_by_file__(self) -> MovieInformation:
-        if self.__information__ is None:
-            if os.path.exists(self.__information_file_path__):
-                print("获取存在的information信息|" + self.__information_file_path__)
+    @status.setter
+    def status(self, value):
+        self.__status__ = value
 
-                with open(self.__information_file_path__, "r", encoding="utf-8") as file:
-                    content_info = json.load(file)
-                    if content_info and content_info.get("resource"):
-                        info = content_info["resource"]
+    def get_information(self, information_folder) -> MovieInformation:
+        information_path = os.path.join(information_folder, self.__information_file_name__)
 
-                        self.__information__ = MovieInformation()
-                        self.__information__.id = info["id"]
-                        self.__information__.title = info["title"]
-                        self.__information__.url = info["url"]
-                        self.__information__.poster = info["poster"]
-                        self.__information__.stills = info["stills"]
-                        self.__information__.torrents = info["torrents"]
+        if os.path.exists(information_path):
+            print("获取存在的information信息|" + information_path)
 
-        return self.__information__
+            with open(information_path, "r", encoding="utf-8") as file:
+                content_info = json.load(file)
+                if content_info and content_info.get("resource"):
+                    info = content_info["resource"]
 
-    def to_json(self, save_information=False):
+                    self.__information__ = MovieInformation()
+                    self.__information__.id = info["id"]
+                    self.__information__.title = info["title"]
+                    self.__information__.url = info["url"]
+                    self.__information__.poster = info["poster"]
+                    self.__information__.stills = info["stills"]
+                    self.__information__.torrents = info["torrents"]
+        return None
+
+    def to_json(self, information=None, save_path=None):
         result = {
             "href": self.__href__,
             "title": self.__title__,
             "key": self.__key__,
             "index": self.__index__,
-            "status": self.__status__
+            # "status": self.__status__
         }
 
-        if self.__path__:
-            result["path"] = self.__path__
+        if save_path:
+            result["path"] = save_path
 
-        if self.__information__ and save_information:
-            result["resource"] = self.__information__.to_json()
+        if information:
+            result["resource"] = information.to_json()
 
         return result
 
-    def download(self):
-        if self.__information__:
-            self.__inspection__()
-        else:
+    def download(self, save_folder):
+        save_path = None
+
+        try:
             javdb_url = self.__href__
             movie_info = None
 
@@ -105,90 +85,48 @@ class Bookmark(object):
                 movie_info = marauder.get_movie(javdb_url)
 
             if movie_info:
-                try:
-                    self.__information__ = movie_info
-                    self.__path__ = os.path.join(self.__group_path__, movie_info.title)
-                    self.__save_information__()
-                    self.__save_stills__()
-                    self.__save_poster__()
+                save_path = os.path.join(save_folder, movie_info.title)
 
-                    TaskPool.append_task(Task(self.__inspection__, args=None, in_queue_delay_seconds=self.__max_inspection_count__()))
-                except Exception as error:
-                    self.__save__("error")
-            else:
-                self.__save__("error")
+                if not os.path.exists(save_path):
+                    Path(save_path).mkdir(exist_ok=True)
 
-    def __save_information__(self):
-        if not os.path.exists(self.__path__):
-            Path(self.__path__).mkdir(exist_ok=True)
+                information_path = os.path.join(save_path, self.__information_file_name__)
 
-        if not self.__information_file_path__:
-            self.__information_file_path__ = os.path.join(self.__path__, self.__information_file_name__)
+                with open(information_path, "w", encoding="utf-8") as file:
+                    json.dump(self.to_json(movie_info, save_path), file, indent=4, ensure_ascii=False)
 
-        with open(self.__information_file_path__, "w", encoding="utf-8") as file:
-            json.dump(self.to_json(True), file, indent=4, ensure_ascii=False)
+                    if movie_info.poster:
+                        HttpClient.download(**{"path": os.path.join(save_path, movie_info.poster["name"]), "url": movie_info.poster["url"]})
 
-    def __save_stills__(self):
-        if self.__information__ and self.__information__.stills and len(self.__information__.stills):
-            self.__append_download_tasks__(self.__information__.stills)
+                    if movie_info.stills and len(movie_info.stills) > 0:
+                        for still in movie_info.stills:
+                            HttpClient.download(**{"path": os.path.join(save_path, still["name"]), "url": still["url"]})
+        except Exception as error:
+            self.delete(save_path)
+            raise error
 
-    def __save_poster__(self):
-        if self.__information__ and self.__information__.poster:
-            self.__append_download_task__(os.path.join(self.__path__, self.__information__.poster["name"]), self.__information__.poster["url"])
+    def inspection(self, information, save_path):
+        try:
+            items = [{"path": os.path.join(save_path, still["name"]), "url": still["url"]} for still in information.stills]
+            items.append({"path": os.path.join(save_path, information.poster["name"]), "url": information.poster["url"]})
 
-    def __append_download_task__(self, name, url):
-        if name and url:
-            file_path = os.path.join(self.__path__, name)
-
-            if not os.path.exists(file_path):
-                self.__inspection_delay_seconds__ = self.__inspection_delay_seconds__ + 1
-
-                down_path = name
-                if self.__path__ not in down_path:
-                    down_path = os.path.join(self.__path__, name)
-
-                task = Task(HttpClient.download, kwargs={"path": down_path, "url": url})
-                TaskPool.append_task(task)
-
-    def __append_download_tasks__(self, items):
-        for item in items:
-            self.__append_download_task__(item["name"], item["url"])
-
-    def __max_inspection_count__(self):
-        if self.__information__ and self.__information__.stills and len(self.__information__.stills):
-            self.__inspection_delay_seconds__ = len(self.__information__.stills)
-        return self.__inspection_delay_seconds__
-
-    def __inspection__(self):
-        if self.__inspection_count__ >= self.__max_inspection_count__():
-            self.__save__("error")
-        else:
-            self.__inspection_count__ = self.__inspection_count__ + 1
-
-            items = [{"path": os.path.join(self.__path__, still["name"]), "url": still["url"]} for still in self.__information__.stills]
-            items.append({"path": os.path.join(self.__path__, self.__information__.poster["name"]), "url": self.__information__.poster["url"]})
-
-            done = True
             if items and len(items) > 0:
                 for item in items:
                     if not os.path.exists(item["path"]):
-                        done = False
-                        self.__append_download_task__(item["path"], item["url"])
+                        HttpClient.download(**{"path": item["path"], "url": item["url"]})
+        except Exception as error:
+            self.delete(save_path)
+            raise error
 
-            if done:
-                self.__save__("done")
-            else:
-                TaskPool.append_task(Task(self.__inspection__, args=None, in_queue_delay_seconds=5))
+    def delete(self, save_path):
+        self.__status__ = "error"
 
-    def __save__(self, status="done"):
-        self.__status__ = status
+        if os.path.exists(save_path):
+            shutil.rmtree(save_path)
 
-        if status != "done":
-            if os.path.exists(self.__path__):
-                shutil.rmtree(self.__path__)
-        else:
-            self.__save_information__()
 
-        TaskPool.append_task(Task(self.__group__.save, args=None))
+            
+
+
 
 
--- modules/service/bookmarks/dustman.py
+++ modules/service/bookmarks/dustman.py
@@ -1,3 +1,4 @@
+import json
 import re
 import os.path
 
@@ -6,7 +7,6 @@ from modules.tools.http_request.proxy import Proxies
 from modules.tools.thread_pools.task_pool import TaskPool
 
 from modules.service.bookmarks.bookmark import Bookmark
-from modules.service.bookmarks.bookmark_group import BookmarkGroup, BookmarkGroups
 
 
 def __get_bookmark_info__(bookmark_html_line):
@@ -55,43 +55,90 @@ def __get_bookmark_info__(bookmark_html_line):
 
 class Dustman(object):
     def __init__(self, bookmarks_html_file_path, bookmark_save_folder=None):
-        self.__bookmarks_html_file_path__ = bookmarks_html_file_path
-        self.__bookmarks_json_file_folder__ = os.path.dirname(bookmarks_html_file_path)
+        self.__html_file_path__ = bookmarks_html_file_path
+        self.__json_file_folder__ = os.path.dirname(bookmarks_html_file_path)
 
-        if bookmark_save_folder is not None:
-            self.__bookmarks_json_file_folder__ = bookmark_save_folder
+        self.__download_item_folder = os.path.join(self.__json_file_folder__, "downloading")
+        self.__done_item_folder__ = os.path.join(self.__json_file_folder__, "done")
 
-        self.__bookmarks_groups__ = BookmarkGroups(self.__bookmarks_json_file_folder__)
+        self.__done_json_file_path__ = os.path.join(self.__json_file_folder__, "done.json")
+        self.__error_json_file_path__ = os.path.join(self.__json_file_folder__, "error.json")
+        self.__open_json_file_path__ = os.path.join(self.__json_file_folder__, "open.json")
 
-    def save(self, group_line_qty=500):
-        bookmarks = self.__get_bookmarks__()
-        bookmarks_group = [bookmarks[i: i + group_line_qty] for i in range(0, len(bookmarks), group_line_qty)]
+        self.__open_items__ = None
+        self.__error_items__ = None
+        self.__done_items__ = None
 
-        index = 0
-
-        for bookmarks_group_items in bookmarks_group:
-            index = index + 1
-            bookmark_group = BookmarkGroup(os.path.join(self.__bookmarks_json_file_folder__, "group_%s" % str(index).zfill(5)))
-            bookmark_group.items = bookmarks_group_items
-            bookmark_group.save()
-
-            self.__bookmarks_groups__.append_group_path(bookmark_group.file_path)
-
-        self.__bookmarks_groups__.save()
+    def save(self):
+        items = self.__get_bookmarks__()
+        with open(self.__open_json_file_path__, "w", encoding="utf-8") as file:
+            json.dump(items, file, indent=4, ensure_ascii=False)
 
     def download(self):
-        TaskPool.set_count(10)
         HttpClient.set_proxies(Proxies())
+        items = self.open_items
 
-        for bookmark_group in self.__bookmarks_groups__.get_items():
-            bookmark_group.download()
+        for item in items:
+            bookmark = Bookmark(**item)
+            item_path = item.get("path")
 
-        TaskPool.join()
-        
+            try:
+                if item_path and os.path.exists(item_path):
+                    information = bookmark.get_information(item_path)
+                    bookmark.inspection(information, item_path)
+                else:
+                    bookmark.download(item_path)
+            except Exception as error:
+                bookmark.status = "error"
+            finally:
+                self.save_all_items(items)
+
+    @property
+    def open_items(self):
+        if self.__open_items__ is None:
+            self.__open_items__ = self.__build_items__(self.__open_json_file_path__)
+        return self.__open_items__
+
+    @property
+    def error_items(self):
+        if self.__error_items__ is None:
+            self.__error_items__ = self.__build_items__(self.__error_json_file_path__)
+        return self.__error_items__
+
+    @property
+    def done_items(self):
+        if self.__done_items__ is None:
+            self.__done_items__ = self.__build_items__(self.__done_json_file_path__)
+        return self.__done_items__
+
+    def save_all_items(self, items):
+        done_items = list(filter(lambda item: item.status == "done", items))
+        error_items = list(filter(lambda item: item.status == "error", items))
+        open_items = list(filter(lambda item: item.status == "open", items))
+
+        self.__save_items_with_status__(done_items, self.__done_json_file_path__)
+        self.__save_items_with_status__(error_items, self.__error_json_file_path__)
+        self.__save_items_with_status__(open_items, self.__open_json_file_path__)
+
+    def __save_items_with_status__(self, items, json_file_path):
+        if items and len(items) > 0:
+            with open(json_file_path, "w", encoding="utf-8") as file:
+                json.dump([item.to_json() for item in items], file, ensure_ascii=False)
+
+    def __build_items__(self, json_file_path):
+        if os.path.exists(json_file_path):
+            with open(json_file_path, "r", encoding="utf-8") as file:
+                result = json.load(file)
+                if result is not None and len(result) > 0:
+                    return result
+                else:
+                    return []
+
     def __get_bookmarks__(self):
         content = []
         line_index = 0
-        with open(self.__bookmarks_html_file_path__, encoding='utf-8', mode='r') as html_file:
+
+        with open(self.__html_file_path__, encoding='utf-8', mode='r') as html_file:
             while True:
                 line = html_file.readline()
                 if not line:
@@ -104,7 +151,7 @@ class Dustman(object):
 
                         item["index"] = line_index
                         item["status"] = "open"
-                        content.append(Bookmark(**item))
+                        content.append(item)
 
         return content
 
