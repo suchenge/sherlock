fa1e86a013d9591a478cb6db2c7efe4eccfadf6a
Vito <vito.su@live.com>
2023-03-22 21:37:53 +0800

    完善bookmark功能

--- modules/service/download/picture/page_factory.py
+++ modules/service/download/picture/page_factory.py
@@ -11,7 +11,7 @@ class PageFactory(object):
     settings = {
         'xiurenji': PageXiuRenJi,
         'xiannvku': PageXinannvku,
-        'xiuren01': PageXiurenba,
+        'xiuren5': PageXiurenba,
         'xrmn': PageXrmn,
         'jpmnb': PageJpmnb,
         'jpxgmn': PageJpxgmn
