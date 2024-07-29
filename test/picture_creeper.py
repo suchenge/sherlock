8fcbd430efeb71610d4585087319d1eea1fa4fe2
Vito Su <vito.su@live.com>
2024-07-29 21:24:59 +0800

    picture_creeper add vpn

--- test/picture_creeper.py
+++ test/picture_creeper.py
@@ -1,5 +1,7 @@
 from modules.service.download.picture.creeper import Creeper
+from modules.tools.http_request.http_client import HttpClient
+from tools.http_request.proxy import Proxies
 
-
+HttpClient.set_proxies(Proxies())
 # 抓取图片程序
 Creeper().run()
