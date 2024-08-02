e1515eafb203961aa4b07c07a48f11334d9875e4
Vito <vitosu@qt-asia.com>
2024-08-02 13:22:59 +0800

    完善图片下载处理方式

--- test/butcher.py
+++ test/butcher.py
@@ -2,16 +2,17 @@ import os
 from ffmpy import FFmpeg
 
 from modules.service.movie_cut.butcher import Butcher
-from tools.common_methods.unity_tools import ffmpeg_execute_path
+from modules.tools.common_methods.unity_tools import ffmpeg_execute_path
 
 path_params = [
     r"/Users/vito/Movies/教学/move/20240515-VIP.mp4"
 ]
 
 
+# 文件合并
+'''
 menu_file_path = r'/Users/vito/Movies/教学/move/20240515-VIP.txt'
 concat_file = os.path.join('/Users/vito/Movies/教学/move', '20240515-VIP.txt')
-
 ff = FFmpeg(
     global_options=['-f', 'concat'],
     inputs={concat_file: ['-safe', '0']},
@@ -22,7 +23,7 @@ ff = FFmpeg(
 print(ff.cmd)
 
 ff.run()
-
+'''
 
 for path in path_params:
     if path is not None and path != '':
