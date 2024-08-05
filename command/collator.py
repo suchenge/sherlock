43de4b3110ff92e43fe9c05f5c6bc1e942f3f980
Vito Su <vito.su@live.com>
2024-08-05 22:28:04 +0800

    marauder javdb fix bug

--- command/collator.py
+++ command/collator.py
@@ -2,11 +2,10 @@ import os
 import sys
 
 # os.system("pause")
-
 cur_path = os.path.abspath(os.path.dirname(__file__))
 module_path = os.path.split(cur_path)[0]
 sys.path.append(module_path)
-# sys.path.append('/Users/vito/Workspace/repositories/python/site-packages')
+
 
 from modules.service.movie_warehouse.collate.collator import Collator
 from modules.tools.exception_container.exception_list import ExceptionList
